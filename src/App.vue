d3575752660dbd7a6b777a5f550d26590ef4b711
Jane <1967363774@qq.com>
2023-02-25 16:03:44 +0800

    v0.6-添加tab导航el-tabs

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-    <ToolPage></ToolPage>
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="画廊" name="first"> <HomePage></HomePage> </el-tab-pane>
+      <el-tab-pane label="工具导航" name="second"> <ToolPage></ToolPage> </el-tab-pane>
+    </el-tabs> 
   </div>
 </template>
 
 <script>
-// import HomePage from './pages/HomePage/index.vue';
+import HomePage from './pages/HomePage/index.vue';
 import ToolPage from './pages/ToolPage/index.vue';
 
 export default {
   name: 'App',
   components: {
+    HomePage,
     ToolPage
+  },
+  data() {
+    return {
+      activeName: 'first'
+    }
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
   }
 }
 </script>
@@ -20,6 +34,7 @@ export default {
 #app {
   background: #f5f5fb;
   height: 100vh;
+  padding: 0 10px;
 }
 body {
   margin: 0;
