d9c7d0ee6281bf567f8d7d0a8dc7b65e6b85be71
Jane <1967363774@qq.com>
2023-02-25 18:17:17 +0800

    v0.8-添加menu导航

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <MenuList class="menu"></MenuList>
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
+<script>
+import MenuList from './components/MenuList/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MenuList
+  }
+}
+</script>
+
 <style lang="less">
+@menuWidth: 300px;
 #app {
-  background: #f5f5fb;
-  height: 100vh;
-  padding: 0 10px;
+  .menu {
+    position: fixed;
+    width: @menuWidth;
+    top: 0;
+    bottom: 0;
+    left: 0;
+  }
+  .content {
+    margin-left: @menuWidth;
+  }
 }
 body {
+  background: #f5f5fb;
   margin: 0;
 }
 </style>
---
+++ src/components/MenuList/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <el-menu default-active="/" 
+                 class="el-menu-vertical"
+                 @select="handSelect">
+            <el-menu-item index="/home">
+                <i class="el-icon-picture-outline"></i>
+                <span slot="title">画廊</span>
+            </el-menu-item>
+            <el-menu-item index="/tool">
+                <i class="el-icon-suitcase-1"></i>
+                <span slot="title">工具导航</span>
+            </el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MenuList',
+    methods: {
+        handSelect(path) {
+            this.$router.push(path);
+        }
+    }
+}
+</script>
+
+<style>
+.el-menu-vertical {
+    height: 100vh;
+}
+</style>
